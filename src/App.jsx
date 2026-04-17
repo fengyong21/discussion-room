2ddac92d5ba067f396bb5c69e8d83567128fd7a8
雪蕊 <30512446+fengyong21@users.noreply.github.com>
2026-04-17 12:45:13 +0800

    init: add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import ChatPage from './pages/ChatPage'
+import DiagnosisPage from './pages/DiagnosisPage'
+import ContentPage from './pages/ContentPage'
+import RankingPage from './pages/RankingPage'
+import NotFoundPage from './pages/NotFoundPage'
+import SettingsPage from './pages/SettingsPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="chat" element={<ChatPage />} />
+          <Route path="diagnosis" element={<DiagnosisPage />} />
+          <Route path="content" element={<ContentPage />} />
+          <Route path="ranking" element={<RankingPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
